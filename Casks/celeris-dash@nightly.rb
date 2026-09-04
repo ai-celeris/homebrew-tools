8e7ca5a9ffd6b963ba381adeb6d35f67c1eba418
Oliver Lade <oliver@marqo.ai>
2026-09-05 04:41:16 +1000

    Update celeris-dash@nightly and celeris-dash-nightly to 0.5.0-src0000002604.c0.nightly.gb3fde232 (#92)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/celeris-dash@nightly.rb
+++ Casks/celeris-dash@nightly.rb
@@ -1,6 +1,6 @@
 cask "celeris-dash@nightly" do
-  version "0.5.0-src0000002597.c0.nightly.g14aa82c7"
-  sha256 "e680af708aab783d3e9f3f0fa16ecaf48beaa5560792aec8d93609c0f9a04ac9"
+  version "0.5.0-src0000002604.c0.nightly.gb3fde232"
+  sha256 "266f1a8c30c10ef1b85089a6fe2e3b80e0cd2b0097fe0b1c96beea038ac81e35"
 
   url "https://downloads.dash.celeris.ai/releases/v#{version}/celeris-dash-#{version}-macos-arm64.zip"
   name "Celeris Dash (nightly)"
