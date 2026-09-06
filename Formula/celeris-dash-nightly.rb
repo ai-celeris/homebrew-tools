90e945e61bc7e668f2a880f952f44c1dc5ea7359
Oliver Lade <oliver@marqo.ai>
2026-09-07 00:19:45 +1000

    Update celeris-dash@nightly and celeris-dash-nightly to 0.5.0-src0000002742.c0.nightly.gcad74225 (#144)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/celeris-dash-nightly.rb
+++ Formula/celeris-dash-nightly.rb
@@ -1,9 +1,9 @@
 class CelerisDashNightly < Formula
   desc "Local desktop dispatcher powered by celeris-1 (nightly channel)"
   homepage "https://dash.celeris.ai/"
-  url "https://downloads.dash.celeris.ai/releases/v0.5.0-src0000002741.c0.nightly.gc97f9a5d/celeris-dash-0.5.0-src0000002741.c0.nightly.gc97f9a5d-linux-x64.AppImage"
-  version "0.5.0-src0000002741.c0.nightly.gc97f9a5d"
-  sha256 "6b865dba4511a7a262ebbf1d74b6670811e9eaee2c5b5dc840adb873d607e2fd"
+  url "https://downloads.dash.celeris.ai/releases/v0.5.0-src0000002742.c0.nightly.gcad74225/celeris-dash-0.5.0-src0000002742.c0.nightly.gcad74225-linux-x64.AppImage"
+  version "0.5.0-src0000002742.c0.nightly.gcad74225"
+  sha256 "d386cc0b77476c1a826b033e01577b5c86de7ee4c858b7d53ece742aab3cee03"
   license :cannot_represent
 
   def install
