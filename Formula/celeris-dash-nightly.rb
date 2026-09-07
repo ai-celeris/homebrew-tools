03d726717ca37f505802762f8dffb6d8e944885a
Oliver Lade <oliver@marqo.ai>
2026-09-08 06:22:42 +1000

    Update celeris-dash@nightly and celeris-dash-nightly to 0.5.0-src0000002838.c0.nightly.gff9fd98e (#204)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/celeris-dash-nightly.rb
+++ Formula/celeris-dash-nightly.rb
@@ -1,9 +1,9 @@
 class CelerisDashNightly < Formula
   desc "Local desktop dispatcher powered by celeris-1 (nightly channel)"
   homepage "https://dash.celeris.ai/"
-  url "https://downloads.dash.celeris.ai/releases/v0.5.0-src0000002836.c0.nightly.g85df6563/celeris-dash-0.5.0-src0000002836.c0.nightly.g85df6563-linux-x64.AppImage"
-  version "0.5.0-src0000002836.c0.nightly.g85df6563"
-  sha256 "983db233d4bba4bedaa6a23b2edfefa217230466180390097cd3728fa576adf5"
+  url "https://downloads.dash.celeris.ai/releases/v0.5.0-src0000002838.c0.nightly.gff9fd98e/celeris-dash-0.5.0-src0000002838.c0.nightly.gff9fd98e-linux-x64.AppImage"
+  version "0.5.0-src0000002838.c0.nightly.gff9fd98e"
+  sha256 "7b9b9f353e54e8138afddddf59c554861d7bc5c38627c69e657fb5e4dcaad99b"
   license :cannot_represent
 
   def install
