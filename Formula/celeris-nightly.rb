3c4de97c205358282e8486a460466d171523450e
Oliver Lade <oliver@marqo.ai>
2026-09-22 07:56:02 +1000

    Update celeris@nightly and celeris-nightly to 0.5.0-src0000003370.c0.nightly.g844c7fb5 (#393)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/celeris-nightly.rb
+++ Formula/celeris-nightly.rb
@@ -1,9 +1,9 @@
 class CelerisNightly < Formula
   desc "Fast assistant that acts on whatever is on your screen (nightly channel)"
   homepage "https://app.celeris.ai/"
-  url "https://downloads.celeris.ai/releases/v0.5.0-src0000003368.c0.nightly.gecbd1f33/celeris-dash-0.5.0-src0000003368.c0.nightly.gecbd1f33-linux-x64.AppImage"
-  version "0.5.0-src0000003368.c0.nightly.gecbd1f33"
-  sha256 "adee37ffbb3c3f0041c9644bf7bc9bef569fcc3e932ec329eee6f19ec7818a13"
+  url "https://downloads.celeris.ai/releases/v0.5.0-src0000003370.c0.nightly.g844c7fb5/celeris-dash-0.5.0-src0000003370.c0.nightly.g844c7fb5-linux-x64.AppImage"
+  version "0.5.0-src0000003370.c0.nightly.g844c7fb5"
+  sha256 "a95132f92956f3f5da580aae8b3e5618ea1bdcb6f4d15e1c3fb963d146c662f2"
   license :cannot_represent
 
   def install
