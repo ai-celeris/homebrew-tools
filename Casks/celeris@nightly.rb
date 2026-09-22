4648e4a6ce0c34a2b87960c5d1e12abb585361c1
Oliver Lade <oliver@marqo.ai>
2026-09-22 17:11:42 +1000

    Update celeris@nightly and celeris-nightly to 0.5.0-src0000003422.c0.nightly.gc1517ec8 (#413)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/celeris@nightly.rb
+++ Casks/celeris@nightly.rb
@@ -1,6 +1,6 @@
 cask "celeris@nightly" do
-  version "0.5.0-src0000003421.c0.nightly.g216cb5ba"
-  sha256 "dd3a5b619c4016253e00cd7601883ebfaeeac46cb41b969724f354f731afea7a"
+  version "0.5.0-src0000003422.c0.nightly.gc1517ec8"
+  sha256 "b246b3fd2872dd7afabbe4f7c4b419c610d8d3ac4abe2f24ace6ad6e2c854d16"
 
   url "https://downloads.celeris.ai/releases/v#{version}/celeris-dash-#{version}-macos-arm64.zip"
   name "Celeris (nightly)"
--- Formula/celeris-nightly.rb
+++ Formula/celeris-nightly.rb
@@ -1,9 +1,9 @@
 class CelerisNightly < Formula
   desc "Fast assistant that acts on whatever is on your screen (nightly channel)"
   homepage "https://app.celeris.ai/"
-  url "https://downloads.celeris.ai/releases/v0.5.0-src0000003421.c0.nightly.g216cb5ba/celeris-dash-0.5.0-src0000003421.c0.nightly.g216cb5ba-linux-x64.AppImage"
-  version "0.5.0-src0000003421.c0.nightly.g216cb5ba"
-  sha256 "bcaa7ad4e793f73bbe24f24a36bc0af19faed430ffbd3ed35c278512c3a1e138"
+  url "https://downloads.celeris.ai/releases/v0.5.0-src0000003422.c0.nightly.gc1517ec8/celeris-dash-0.5.0-src0000003422.c0.nightly.gc1517ec8-linux-x64.AppImage"
+  version "0.5.0-src0000003422.c0.nightly.gc1517ec8"
+  sha256 "1d7954954cb24d255f4a9d853c0e77258164f5cac70e89e710aed766fca933b2"
   license :cannot_represent
 
   def install
